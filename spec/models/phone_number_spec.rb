e02e2d4a270d66712d94eb1816e8d0740e3aecfd
rossedfort <rossedfort@gmail.com>
2015-10-29 21:21:03 -0600

    all phone number validation tests passing

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, type: :model do
-  let(:phone_number) { PhoneNumber.new(number: 9086989024) }
+  let(:phone_number) { PhoneNumber.new(number: 9086989024, person_id: 1) }
 
   it 'is valid' do
     expect(phone_number).to be_valid
@@ -9,5 +9,9 @@ RSpec.describe PhoneNumber, type: :model do
   it 'is invalid without a number' do
     phone_number.number = nil
     expect(phone_number).to_not be_valid
+  end
+  it 'must have a reference to a person' do
+    phone_number.person_id = nil
+    expect(phone_number).not_to be_valid
   end
 end
