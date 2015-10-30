0d567dba8c2be4bfed7dfe9c974c94039b8ca9a8
rossedfort <rossedfort@gmail.com>
2015-10-29 21:15:55 -0600

    phone number validation tests passing - 16 pending

--- spec/models/phone_number_spec.rb
+++ spec/models/phone_number_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe PhoneNumber, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:phone_number) { PhoneNumber.new(number: 9086989024) }
+
+  it 'is valid' do
+    expect(phone_number).to be_valid
+  end
+  it 'is invalid without a number' do
+    phone_number.number = nil
+    expect(phone_number).to_not be_valid
+  end
 end
