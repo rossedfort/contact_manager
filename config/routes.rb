1f462dcf2386a2b6069b1642ed6fcd21811673c5
rossedfort <rossedfort@gmail.com>
2015-10-29 20:58:10 -0600

    Generate phone number and associate with person

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :phone_numbers
   resources :people
   resources :people
   # The priority is based upon order of creation: first created -> highest priority.
