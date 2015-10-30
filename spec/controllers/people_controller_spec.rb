bcd83c742ebbbed3c2e41190252f8e34ede89c54
rossedfort <rossedfort@gmail.com>
2015-10-29 20:28:17 -0600

    app actually working - finally, added valid attributes to people controller spec

---
+++ spec/controllers/people_controller_spec.rb
@@ -0,0 +1,133 @@
+require 'rails_helper'
+
+RSpec.describe PeopleController, type: :controller do
+  let(:valid_attributes) {
+    {first_name: 'Ross', last_name: 'Edfort'}
+  }
+
+  let(:invalid_attributes) {
+    skip("Add a hash of attributes invalid for your model")
+  }
+  let(:valid_session) { {} }
+
+  describe "GET #index" do
+    it "assigns all people as @people" do
+      person = Person.create! valid_attributes
+      get :index, {}, valid_session
+      expect(assigns(:people)).to eq([person])
+    end
+  end
+
+  describe "GET #show" do
+    it "assigns the requested person as @person" do
+      person = Person.create! valid_attributes
+      get :show, {:id => person.to_param}, valid_session
+      expect(assigns(:person)).to eq(person)
+    end
+  end
+
+  describe "GET #new" do
+    it "assigns a new person as @person" do
+      get :new, {}, valid_session
+      expect(assigns(:person)).to be_a_new(Person)
+    end
+  end
+
+  describe "GET #edit" do
+    it "assigns the requested person as @person" do
+      person = Person.create! valid_attributes
+      get :edit, {:id => person.to_param}, valid_session
+      expect(assigns(:person)).to eq(person)
+    end
+  end
+
+  describe "POST #create" do
+    context "with valid params" do
+      it "creates a new Person" do
+        expect {
+          post :create, {:person => valid_attributes}, valid_session
+        }.to change(Person, :count).by(1)
+      end
+
+      it "assigns a newly created person as @person" do
+        post :create, {:person => valid_attributes}, valid_session
+        expect(assigns(:person)).to be_a(Person)
+        expect(assigns(:person)).to be_persisted
+      end
+
+      it "redirects to the created person" do
+        post :create, {:person => valid_attributes}, valid_session
+        expect(response).to redirect_to(Person.last)
+      end
+    end
+
+    context "with invalid params" do
+      it "assigns a newly created but unsaved person as @person" do
+        post :create, {:person => invalid_attributes}, valid_session
+        expect(assigns(:person)).to be_a_new(Person)
+      end
+
+      it "re-renders the 'new' template" do
+        post :create, {:person => invalid_attributes}, valid_session
+        expect(response).to render_template("new")
+      end
+    end
+  end
+
+  describe "PUT #update" do
+    context "with valid params" do
+      let(:new_attributes) {
+        skip("Add a hash of attributes valid for your model")
+      }
+
+      it "updates the requested person" do
+        person = Person.create! valid_attributes
+        put :update, {:id => person.to_param, :person => new_attributes}, valid_session
+        person.reload
+        skip("Add assertions for updated state")
+      end
+
+      it "assigns the requested person as @person" do
+        person = Person.create! valid_attributes
+        put :update, {:id => person.to_param, :person => valid_attributes}, valid_session
+        expect(assigns(:person)).to eq(person)
+      end
+
+      it "redirects to the person" do
+        person = Person.create! valid_attributes
+        put :update, {:id => person.to_param, :person => valid_attributes}, valid_session
+        expect(response).to redirect_to(person)
+      end
+    end
+
+    context "with invalid params" do
+      it "assigns the person as @person" do
+        person = Person.create! valid_attributes
+        put :update, {:id => person.to_param, :person => invalid_attributes}, valid_session
+        expect(assigns(:person)).to eq(person)
+      end
+
+      it "re-renders the 'edit' template" do
+        person = Person.create! valid_attributes
+        put :update, {:id => person.to_param, :person => invalid_attributes}, valid_session
+        expect(response).to render_template("edit")
+      end
+    end
+  end
+
+  describe "DELETE #destroy" do
+    it "destroys the requested person" do
+      person = Person.create! valid_attributes
+      expect {
+        delete :destroy, {:id => person.to_param}, valid_session
+      }.to change(Person, :count).by(-1)
+    end
+
+    it "redirects to the people list" do
+      person = Person.create! valid_attributes
+      delete :destroy, {:id => person.to_param}, valid_session
+      expect(response).to redirect_to(people_url)
+    end
+  end
+
+end
