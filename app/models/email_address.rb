ae55c0347745c6e9d2983ef7b6bed368e60dac13
rossedfort <rossedfort@gmail.com>
2015-10-31 10:35:17 -0600

    added extra validation for email address having a person id - incliuding tests

--- app/models/email_address.rb
+++ app/models/email_address.rb
@@ -1,5 +1,5 @@
 class EmailAddress < ActiveRecord::Base
   belongs_to :person
 
-  validates_presence_of :address
+  validates_presence_of :address, :person_id
 end
--- spec/models/email_address_spec.rb
+++ spec/models/email_address_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe EmailAddress, type: :model do
-  let(:email_address) { EmailAddress.new(address: 'example@example.com') }
+  let(:email_address) { EmailAddress.new(address: 'example@example.com', person_id: 1) }
 
   it "is valid" do
     expect(email_address).to be_valid
@@ -11,4 +11,10 @@ RSpec.describe EmailAddress, type: :model do
     email_address.address = nil
     expect(email_address).to_not be_valid
   end
+
+  it "is not valid without a person_id" do
+    email_address.person_id = nil
+    expect(email_address).to_not be_valid
+  end
+
 end
