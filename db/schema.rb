4ab1e4063c41386bfb3321b4adc79f09a9e58bd5
rossedfort <rossedfort@gmail.com>
2015-10-31 14:00:10 -0600

    validation on company name feature implemented

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151031160232) do
+ActiveRecord::Schema.define(version: 20151031195455) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "companies", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "email_addresses", force: :cascade do |t|
     t.string   "address"
---
+++ spec/models/company_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe Company, type: :model do
+  let(:company) {Company.create(name: 'Turing')}
+
+  it "is valid" do
+    expect(company).to be_valid
+  end
+
+  it "is not valid without a name" do
+    company.name = nil
+    expect(company).to_not be_valid
+  end
+
+end
